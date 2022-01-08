96bf68d6dd31b7d70546044ade72a1b872ebaa06
Seun Oshitade <seun.oshitade@gmail.com>
2022-01-08 16:29:00 +0000

    Incorporated Lodash!

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -10,23 +11,30 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-const posts = [];   
+const posts = [];
 
 app.get("/", (req, res) => {
   res.render("home", {
-    homeStartingContent: homeStartingContent, 
-    posts: posts});
+    homeStartingContent: homeStartingContent,
+    posts: posts
+  });
 })
 
-app.get("/about", (req, res)=> {
-  res.render("about", {aboutContent: aboutContent})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    aboutContent: aboutContent
+  })
 })
 
 app.get("/contact", (req, res) => {
-  res.render("contact", {contactInfo: contactContent} )
+  res.render("contact", {
+    contactInfo: contactContent
+  })
 })
 
 app.get("/compose", (req, res) => {
@@ -41,10 +49,19 @@ app.post("/compose", (req, res) => {
   res.redirect("/")
 })
 
-app.get("/posts/:postName", (req,res)=>{
-  console.log(req.params.postName);
+app.get("/posts/:postName", (req, res) => {
+  const requestedTitle = _.lowerCase(req.params.postName);
+  for (let entry of posts) {
+    const storedTitle = _.lowerCase(entry.title);
+    if (requestedTitle === storedTitle) {
+      console.log("Match found!")
+    }
+  }
+
+  res.redirect("/compose")
+
 })
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
